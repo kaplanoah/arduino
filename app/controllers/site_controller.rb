4fefce9a4b960259fa5a9d8ce01c46293c4b3ed9
Noah Kaplan <noahkaplan4@gmail.com>
2014-03-21 12:18:31 -0700

    cleaned up and commented site controller code

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,18 +2,23 @@ class SiteController < ApplicationController
   
   def display
     arduino = ArduinoFirmata.connect ARGV.shift
-    @message = arduino.analog_read 0
+    @light = arduino.analog_read 0
 
     respond_to do |f|
       f.html { render }
-      f.json { render :json => { :message => @message } }
+      f.json { render :json => { :light => @light } }
     end
   end
 
   def upload
     arduino = ArduinoFirmata.connect ARGV.shift
+
+    # initialize constants
     arduino.pin_mode 13, ArduinoFirmata::OUTPUT
+    
+    # setup
     arduino.digital_write 13, true
+
     redirect_to :root
   end
 
