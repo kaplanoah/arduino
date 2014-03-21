b734dfc6061fabf221b2b5ed104e09732134c73b
Noah Kaplan <noahkaplan4@gmail.com>
2014-03-20 22:29:01 -0700

    added trail ruby code to blink an led on arduino board

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,13 @@ class SiteController < ApplicationController
   
   def display
     @message = "Hello world"
+    
+    # arduino = ArduinoFirmata.connect ARGV.shift
+    # arduino.pin_mode 13, ArduinoFirmata::OUTPUT
+    # arduino.digital_write 13, true
+    # sleep 1
+    # arduino.digital_write 13, false
+    # sleep 1
 
     respond_to do |f|
       f.html { render }
