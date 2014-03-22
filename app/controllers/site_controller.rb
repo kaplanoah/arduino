cdab5e45de73b9994050d4b34d191560489ca93e
Noah Kaplan <noahkaplan4@gmail.com>
2014-03-21 17:41:22 -0700

    removed arduino connections from site controller methods and made it one global variable in the class

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,11 @@
 class SiteController < ApplicationController
   
-  def display
-    arduino = ArduinoFirmata.connect ARGV.shift
-    @light = arduino.analog_read 0
-    @temperature = arduino.analog_read 1
+  $arduino = ArduinoFirmata.connect ARGV.shift
 
+  def display
+    @light = $arduino.analog_read 0
+    @temperature = $arduino.analog_read 1
+    
     respond_to do |f|
       f.html { render }
       f.json { render :json => { :light => @light, :temperature => @temperature } }
@@ -12,13 +13,11 @@ class SiteController < ApplicationController
   end
 
   def upload
-    arduino = ArduinoFirmata.connect ARGV.shift
-
     # initialize constants
-    arduino.pin_mode 13, ArduinoFirmata::OUTPUT
+    $arduino.pin_mode 13, ArduinoFirmata::OUTPUT
     
     # setup
-    arduino.digital_write 13, true
+    $arduino.digital_write 13, true
 
     redirect_to :root
   end
