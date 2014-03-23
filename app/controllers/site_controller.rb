6a93d664150e2b7c4a727a4f4f0ff88a7828bf06
Noah Kaplan <noahkaplan4@gmail.com>
2014-03-22 23:07:00 -0700

    change red and green also, initialize digital pin 2 as sound input, added volume update function and history buffer

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,9 +2,13 @@ class SiteController < ApplicationController
   
   def display
     @light = ARDUINO.analog_read 0
+    sleep 0.01
     @temperature = ARDUINO.analog_read 1
+    sleep 0.01
     @sound = ARDUINO.digital_read 2
+    sleep 0.01
     @volume = ARDUINO.analog_read 2
+    sleep 0.01
     
     respond_to do |f|
       f.html { render }
--- config/initializers/arduino.rb
+++ config/initializers/arduino.rb
@@ -2,6 +2,7 @@ ARDUINO = ArduinoFirmata.connect ARGV.shift
 
 # initialize constants
 ARDUINO.pin_mode 13, ArduinoFirmata::OUTPUT
+ARDUINO.pin_mode 2, ArduinoFirmata::INPUT
 
 # setup
 ARDUINO.digital_write 13, true
